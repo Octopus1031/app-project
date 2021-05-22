6812ecc2b7a97037cd3bb369f4379edcd03712f4
yaYa13360 <83057633+yaYa13360@users.noreply.github.com>
2021-05-22 23:46:53 +0800

    yayaCount

---
+++ final_project/app/src/main/java/com/example/final_project/XYSet.java
@@ -0,0 +1,18 @@
+package com.example.final_project;
+
+public class XYSet {
+    int x,y;
+
+    public int getX() {
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+}
